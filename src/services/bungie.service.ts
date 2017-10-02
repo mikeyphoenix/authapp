a4073a04639c7bbfee6b786d1db23212abde9676
Michael Jacobs <michaeljacobs@pnp.co.za>
2017-10-02 10:30:25 +0200

    first commit

---
+++ src/services/bungie.service.ts
@@ -0,0 +1,6 @@
+import { Injectable } from '@angular/core';
+import { LoadingController } from 'ionic-angular';
+import { Http, Headers, Response, RequestOptions } from '@angular/http';
+import { InAppBrowser } from '@ionic-native/in-app-browser';
+import { Storage } from '@ionic/storage';
+import { Observable } from 'rxjs/Observable';
